da5f3d56a9358123460080f9be2ef780531412ea
Samim Örsoğlu <151220164048@ogrenci.ogu.edu.tr>
2022-02-28 18:18:56 +0300

    PersonelController eklendi.

---
+++ Deneme1.API/Controllers/PersonelsController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Deneme1.Core.Model;
+using Deneme1.Service.DTOs;
+using Deneme1.Service.IService;
+using Deneme1.Service.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Deneme1.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonelsController : ControllerBase
+    {
+        private readonly IPersonelService _personelService;
+        private readonly IMapper _mapper;
+        public PersonelsController(IPersonelService personelService, IMapper mapper)
+        {
+            _personelService = personelService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var personels= await _personelService.GetAllAsync();
+            return Ok(_mapper.Map<IEnumerable<PersonelDto>>(personels));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var personel = await _personelService.GetByIdAsync(id);
+            return Ok(_mapper.Map<PersonelDto>(personel));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(PersonelDto personelDto)
+        {
+            var newPersonel = await _personelService.AddAsync(_mapper.Map<Personel>(personelDto));
+            return Created(string.Empty, _mapper.Map<PersonelDto>(newPersonel));
+        }
+
+        [HttpPut]
+        public IActionResult Update(PersonelUpdateDto personelDto)
+        {
+            var personel = _personelService.Update(_mapper.Map<Personel>(personelDto));
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var personel = await _personelService.GetByIdAsync(id);
+            _personelService.Remove(personel);
+            return NoContent();
+        }
+    }
+}
